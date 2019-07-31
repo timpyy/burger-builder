1cd0acd0a922e8cf99751bb3ca526cc8d56b0974
Tim Pornyuenyong <teakpyy@gmail.com>
2019-07-31 18:15:39 -0400

    Implemented Burger Components, Made ingredient output dynamic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+
 //import "./App.module.css";
 
 function App() {
   return (
     <div>
-      <p>Test</p>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
   );
 }
